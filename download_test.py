3c4dfd5befec62de00656a3857b7b75cbd3a7e96
amunozj <amunozj@boulder.swri.edu>
2023-02-21 18:38:15 -0700

    Add checks for instrument, wavelength, and start the cadence

--- download_test.py
+++ download_test.py
@@ -2,6 +2,7 @@ import unittest
 from downloader import Downloader
 from urllib.parse import urlparse
 import datetime
+import re
 
 class DownloaderTest(unittest.TestCase):
 
@@ -9,7 +10,10 @@ class DownloaderTest(unittest.TestCase):
         email = '.team_red@hotmail.com'
         sdate = '2010-12-21' # '2023-02-14'
         edate = '2011-12-21' # '2023-02-14'
-        self.downloader = Downloader(email, sdate, edate)
+        wavelength = None
+        instrument = "HMI"
+        cadence = '6h 3m 5s'
+        self.downloader = Downloader(email, sdate, edate, wavelength, instrument, cadence)
 
     def test_checkEmail(self):
         self.assertIsNotNone(self.downloader)
@@ -24,6 +28,23 @@ class DownloaderTest(unittest.TestCase):
     def test_checkEndDate(self):
         self.assertIsNotNone(self.downloader)
         self.assertTrue(self.downloader.edate > self.downloader.sdate and self.downloader.edate < datetime.date.today() )
+
+    def test_checkaiaWavelength(self):
+        if(self.downloader.instrument == "aia"):
+            self.assertIsNotNone(self.downloader.wavelength)
+            self.assertIsInstance(self.downloader.wavelength, int)
+            self.assertTrue(self.downloader.wavelength in self.downloader.validwavelengths)
+    
+    def test_checkInstrument(self):
+        self.assertIsNotNone(self.downloader.instrument)
+        self.assertTrue(self.downloader.instrument in self.downloader.validinstruments)
+
+    def test_checkCadence(self):
+        self.assertIsNotNone(self.downloader.cadence)
+        # self.assertTrue(self.downloader.cadence[-1] in self.downloader.validcadence)
+        self.assertTrue(self.downloader.cadence.endswith(tuple(self.downloader.validcadence)))
+        m = re.search("\d+\w", self.downloader.cadence)
+        self.assertTrue(m)
 
 
 
--- downloader.py
+++ downloader.py
@@ -5,11 +5,16 @@ class Downloader:
     # def __init__(self):
     #     ...
 
-    def __init__(self, email:str=None, sdate:str=None, edate:str=None):
+    def __init__(self, email:str=None, sdate:str=None, edate:str=None, wavelength:int=None, instrument:str = None, cadence:str = None):
         self.email = email
         self.sdate = datetime.date.fromisoformat(sdate)
         self.edate = datetime.date.fromisoformat(edate)
-        
+        self.instrument = instrument.lower()
+        self.validinstruments = ["aia", "hmi"]
+        self.wavelength = wavelength
+        self.validwavelengths = [1700, 4500, 1600, 304, 171, 193, 211, 335, 94, 131]
+        self.cadence = cadence
+        self.validcadence = ['s', 'm', 'h', 'd']        
 
 
 
