9340bcf1ba94d92624e59cf58455474822c8c606
David Stone <wovenbone@gmail.com>
2023-04-18 16:21:13 -0700

    Working w/ multiple wavelengths on JPG.

--- downloader.py
+++ downloader.py
@@ -170,18 +170,18 @@ class Downloader:
         files = export_request["download"]
         records = export_request["record"]
 
-        for file, record in zip(files, records):
+        for file, record in zip(files, records): # need to adjust RegEx still.
             file = file.replace("\\", "/").split("/")[-1]
             print(">>>>>>>>>>", file, record)
             instrument = re.search(r"[a-z]+", file)
             date = re.search(r"\d+", file)
-            resolution = re.search(r"4k", file) # NEED TO FIX THIS (not using RegEx - dummy statement)
-            hhmmss = re.search(r"(_)(\d+)(_)", file)
-            fileType = re.search(r"(jpg|fits)", file) # Spikes files too.
+            # resolution = re.search(r"4k", file) # NEED TO FIX THIS (not using RegEx - dummy statement)
+            hhmmss = re.search(r"(_)(\d+\w)(.)", file)
+            fileType = re.search(r"(jpg|fits)", file) # Need spikes files too.
             wavelength = re.search(r"(\]\[(\d+))", record)
             # Rename file name to this format: YYYYMMDD_HHMMSS_RESOLUTION_INSTRUMENT.[filetype]
 
-            newFileName = date.group() + '_' + hhmmss.group(2) + '_' + instrument.group() + "_" + resolution.group() + '_' + wavelength.group(2) + '.' + fileType.group()
+            newFileName = date.group() + '_' + hhmmss.group(2) + '_' + instrument.group() + "_" + wavelength.group(2) + '_' + '4k' + '.' + fileType.group()
             # print(newFileName) # for testing.
             # rename file.
             os.rename(os.path.join(self.path, file), os.path.join(self.path, newFileName))
