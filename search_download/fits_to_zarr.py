1595100c53e5fb4c4de56b0f9260872fe1440975
Andrés Muñoz-Jaramillo <andres.munoz@swri.org>
2023-08-13 16:46:04 -0600

    Create zarr database with both AIA and HMI

--- search_download/fits_to_zarr.py
+++ search_download/fits_to_zarr.py
@@ -9,9 +9,10 @@ import matplotlib.image
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
-from tqdm.contrib.concurrent import process_map
 
-from search_download.utils.utils import loadMapStack
+from search_download.utils.utils import loadMapStack, loadMap
+import zarr
+from numcodecs import Blosc
 
 # Initialize Python Logger
 logging.basicConfig(format='%(levelname)-4s '
@@ -22,77 +23,35 @@ LOG = logging.getLogger()
 LOG.setLevel(logging.INFO)
 
 
-
-def load_map_stack(aia_stack,
-                    aia_preprocessing=True,
-                    calibration='auto',
-                    normalization='asinh',
-                    fix_radius_padding=None,
-                    resolution=None,
-                    remove_nans=True,
-                    percentile_clip=0.25,
-                    zarr_outpath=None,
-                    file_format=None):
-    # Extract filename from index_aia_i (remove aia_path)
-
-    filename = aia_stack[0].replace('\\', '/').split('/')[-1].split('aia')[0]+'aia'
-    for file in aia_stack:
-        filename = filename + '_' + file.replace('\\', '/').split('/')[-1].split('_')[3]
-    filename = filename + '.' + file_format
-
-    output_file = zarr_outpath + '/' + filename
-
-    if exists(output_file):
-        LOG.info(f'{filename} exists.')
-    else:
-        aia_stack = loadMapStack(aia_stack,
-                            aia_preprocessing=aia_preprocessing,
-                            calibration=calibration,
-                            normalization=normalization,
-                            fix_radius_padding=fix_radius_padding,
-                            resolution=resolution,
-                            remove_nans=remove_nans,
-                            percentile_clip=percentile_clip)
-        # Save stack
-        if file_format=='npy':
-            np.save(output_file, aia_stack)
-        if file_format=='jpg':
-            aia_stack = aia_stack.transpose(1, 2, 0)
-            aia_stack[aia_stack<0] = 0
-            aia_stack[aia_stack>1] = 1
-            matplotlib.image.imsave(output_file, aia_stack, vmin=0, vmax=1)
-
-
-    return output_file
-
-
 def parse_args():
     # Commands 
     p = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    p.add_argument('--aia_path', dest='aia_path', type=str, default="/mnt/data",
-                   help='aia_path')
+    p.add_argument('--aia_path', dest='aia_path', type=str, default="/mnt/aia_data",
+                   help='aia path')
+    p.add_argument('--hmi_path', dest='hmi_path', type=str, default="/mnt/hmi_data",
+                   help='hmi path')
     p.add_argument('--matches', dest='matches', type=str,
                    default="/mnt/data/aia_matches_171_211_304.csv",
                    help='multi-wavelength matches')
     p.add_argument('--zarr_outpath', dest='zarr_outpath', type=str,
                    default="/mnt/data_out",
                    help='Zarr out path')
-    p.add_argument('--wavelength_order', type=str,
-                        nargs='+', default=None,
-                        help='Order in which to stack the files, needs to contain only available wavelengths')
     p.add_argument('--aia_preprocessing', dest='aia_preprocessing', action='store_true', 
                    help='Whether to pre-process AIA or simply load the image')
-    p.add_argument('--calibration', dest='calibration', type=str,
+    p.add_argument('--aia_calibration', dest='aia_calibration', type=str,
                    default="aiapy",
                    help="calibration mode for AIAPrepEditor")
+    p.add_argument('--aia_normalization', dest='aia_normalization', type=str,
+                   default="asinh",
+                   help="whether to use 'asinh', 'power', 'linear', or 'none' normalization for aia")    
     p.add_argument('--fix_radius_padding', dest='fix_radius_padding', type=float, default=None, 
                    help='How far from the solar limb to place the edge of the image')
     p.add_argument('--resolution', dest='resolution', type=int, default=None, 
                    help='Target resolution in pixels of 2*(1+fix_radius_padding) solar radii')
     p.add_argument('--remove_nans', action='store_true', 
                    help='change nans and inf for zero')
-    p.add_argument('--percentile_clip', dest='percentile_clip', type=float, default=0.25, 
+    p.add_argument('--percentile_clip', dest='percentile_clip', type=float, default=None, 
                    help='clipping of the hottest pixels to the 100-percentile_clip percentile')
     p.add_argument('--debug', action='store_true',
                    help='Only process a few files (10)')
@@ -103,15 +62,13 @@ def parse_args():
 if __name__ == "__main__":
     # Parser
     args = parse_args()
-    # eve_path = args.eve_path
+    hmi_path = args.hmi_path
     aia_path = args.aia_path
     matches = args.matches
     zarr_outpath = args.zarr_outpath
-    file_format = args.file_format
-    wavelength_order = args.wavelength_order
     aia_preprocessing = args.aia_preprocessing
-    calibration = args.calibration
-    normalization = args.normalization
+    aia_calibration = args.aia_calibration
+    aia_normalization = args.aia_normalization
     fix_radius_padding = args.fix_radius_padding
     resolution = args.resolution
     remove_nans = args.remove_nans
@@ -126,43 +83,79 @@ if __name__ == "__main__":
 
     # Extract filenames for stacks
     aia_columns = []
-    for wl in wavelength_order:
-        for col in matches.columns:
-            if wl in col:
-                aia_columns.append(col)
+    for col in matches.columns:
+        if 'aia' in col:
+            aia_columns.append(col)
 
-    aia_files = []
-    for index, row in tqdm(matches.iterrows()):
-        aia_files.append(row[aia_columns].tolist())  # (channel, files)
+    aia_files = None
+    if len(aia_columns) > 0:
+        aia_files = []
+        for index, row in tqdm(matches.iterrows(), total=matches.shape[0], desc='Turning aia dataframe columns into stacks'):
+            aia_files.append(row[aia_columns].tolist())  # (channel, files)    
 
-    # Path for output
-    os.makedirs(zarr_outpath, exist_ok=True)
+    hmi_columns = []
+    for col in matches.columns:
+        if 'hmi' in col:
+            hmi_columns.append(col)            
+
+    hmi_files = None
+    if len(hmi_columns) > 0:
+        hmi_files = []
+        for index, row in tqdm(matches.iterrows(), total=matches.shape[0], desc='Turning hmi dataframe columns into stacks'):
+            hmi_files.append(row[hmi_columns].tolist())
 
     # Initialize zarr
-    store = zarr.DirectoryStore(target+'sdomlv2_2011.zarr')
+    store = zarr.DirectoryStore(zarr_outpath)
     compressor = Blosc(cname='zstd', clevel=5, shuffle=Blosc.BITSHUFFLE)
-    root = zarr.group(store=store,overwrite=True)    
+    root = zarr.group(store=store, overwrite=True)
 
-    # Stacks
-    print('Saving stacks')
-    partial_load_map_stack = partial(load_map_stack,
-                                    aia_preprocessing=aia_preprocessing,
-                                    calibration=calibration,
-                                    normalization=normalization,
-                                    fix_radius_padding=fix_radius_padding,
-                                    resolution=resolution,
-                                    remove_nans=remove_nans,
-                                    percentile_clip=percentile_clip,
-                                    zarr_outpath=zarr_outpath,
-                                    file_format=file_format)
-    converted_file_paths = process_map(partial_load_map_stack, aia_files, max_workers=None, chunksize=5)
+    if len(aia_columns) > 0:
+        dataset_name = 'aia_' + '_'.join([column.split('files_aia')[1] for column in aia_columns])
+        if len(hmi_columns) > 0:
+            dataset_name = dataset_name + '_hmilos'
+    elif len(hmi_columns) > 0:
+        dataset_name = 'hmilos'
 
-    # Save
-    if debug:
-        matches = matches.loc[0:len(converted_file_paths), :]
+    sdo_stacks = root.create_dataset(dataset_name, 
+                            shape=(matches.shape[0], len(aia_columns) + len(hmi_columns), resolution, resolution), 
+                            chunks=(1, None, None, None), 
+                            dtype='f4',
+                            compressor=compressor)
 
-    print('Saving Matches')
-    matches['aia_stack'] = converted_file_paths
-    matches_output = args.matches.replace('\\','/').split('/')[-1].replace('.csv','_processed.csv')
-    matches_output = f'{zarr_outpath}/{matches_output}'
-    matches.to_csv(matches_output, index=False)
+    # Processing AIA stacks
+    partial_load_map_stack = partial(loadMapStack,
+                            aia_preprocessing=aia_preprocessing,
+                            calibration=aia_calibration,
+                            normalization=aia_normalization,
+                            fix_radius_padding=fix_radius_padding,
+                            resolution=resolution,
+                            remove_nans=remove_nans,
+                            percentile_clip=percentile_clip,
+                            return_meta=True)
+
+    for i, file_stack in tqdm(enumerate(aia_files), total=len(aia_files), desc='Processing AIA stacks'):
+        try:
+            # open file
+            aia_stack, aia_meta = partial_load_map_stack(file_stack)
+            # for key in data.meta:
+            #     if key not in HEADER_FIELD_IGNORE:
+            #         vars()[key].append(data.meta[key])
+
+            sdo_stacks[i, 0:len(aia_columns), :, :] = aia_stack  
+        except Exception as e:
+            print(e)
+
+    for i, file_stack in tqdm(enumerate(hmi_files), total=len(hmi_files), desc='Processing HMI stacks'):
+        try:
+            # open file
+            hmi_map = loadMap(file_stack[0], resolution=resolution, fix_radius_padding=fix_radius_padding, zero_outside_disk=True)
+
+            if remove_nans:
+                hmi_map.data[np.isnan(hmi_map.data)] = 0
+                hmi_map.data[np.isinf(hmi_map.data)] = 0                
+
+            sdo_stacks[i, len(aia_columns), :, :] = hmi_map.data
+
+        except Exception as e:
+            print(e)              
+    
