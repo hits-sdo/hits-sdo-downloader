5a626093c256e6fead63e418ea2c378ab4bcc453
Andrés Muñoz-Jaramillo <andres.munoz@swri.org>
2023-10-08 14:17:20 -0600

    Fix irradiance in AIA normalization
    Define Arcsinh stretch and add documentation
    
    Add zarr to jpg configuration for debugging
    
    Add test to make sure all jpgs where created
    
    Add tqdm monitoring for calculations and parser options to run command line
    
    Reduce size of plot aia hmi notebook

--- search_download/tests/zarr_to_jpg_test.py
+++ search_download/tests/zarr_to_jpg_test.py
@@ -1,7 +1,7 @@
 import unittest
 import os
 from search_download.zarr_to_jpg import ZarrToJpg
-
+import glob
 
 class ZarrToJpgTest(unittest.TestCase):
     """
@@ -9,9 +9,11 @@ class ZarrToJpgTest(unittest.TestCase):
     """
 
     def setUp(self):
-        aia_path = "/d0/euv/aia/preprocessed/aia_hmi_stacks_2010_2023_1d_full.zarr"
+        aia_path = "/d0/euv/aia/preprocessed/aia_hmi_stacks_2010_2023_1d_full_ir.zarr"
         stack_outpath = "/d0/euv/aia/preprocessed"
-        wavelength_order = [211, 193, 171]
+        # wavelength_order = [211, 193, 171]
+        # wavelength_order = [304, 211, 171]
+        wavelength_order = [335, 193, 94]
         debug = True
         self.zarr_to_jpg = ZarrToJpg(
             aia_path=aia_path,
@@ -21,28 +23,58 @@ class ZarrToJpgTest(unittest.TestCase):
         )
 
     def test_check_class(self):
-        """ """
+        """
+            Check that the class exists
+        """
         self.assertIsNotNone(self.zarr_to_jpg)
 
     def test_data_exists(self):
+        """
+            Check that the zarr array has data
+        """
         self.assertIsNotNone(self.zarr_to_jpg.data)
 
     def test_array_dimensions(self):
+        """
+            Check that the zarr array has the right dimensions
+        """
         self.assertEqual(len(self.zarr_to_jpg.data.aia_hmi.shape), 4)
         self.assertEqual(self.zarr_to_jpg.aia_slice.shape[1], 3)
         self.assertTrue(
             self.zarr_to_jpg.debug and self.zarr_to_jpg.aia_slice.shape[0] == 10
         )
 
+    def test_statistics(self):
+        """
+            Check that the histogram, cumulative, and percentile dictionaries have been created
+        """
+        self.assertIsNotNone(self.zarr_to_jpg.histogram_dict)
+        self.assertIsNotNone(self.zarr_to_jpg.cumsum_dict)
+        self.assertIsNotNone(self.zarr_to_jpg.percentile_dict)
+
+    def test_normalization(self):
+        """
+            Check that the arcsinh stretch normalization has been initialized
+        """
+        self.assertIsNotNone(self.zarr_to_jpg.sdo_asinh_norms)
+
     def test_output(self):
+        """'
+            Check that the program is generating jpgs
+        """
         self.assertTrue(os.path.exists(self.zarr_to_jpg.stack_outpath))
-        self.zarr_to_jpg.save_jpg(0)
+        self.zarr_to_jpg.save_jpgs()
         self.assertTrue(
             any(
                 File.endswith(".jpg")
                 for File in os.listdir(self.zarr_to_jpg.stack_outpath)
             )
         )
+        files = glob.glob(f"{self.zarr_to_jpg.stack_outpath}/*.jpg")
+        self.assertEqual(len(files), 10)
+
+
+
 
 
 if __name__ == "__main__":
--- search_download/zarr_to_jpg.py
+++ search_download/zarr_to_jpg.py
@@ -13,6 +13,12 @@ import pandas as pd
 from tqdm import tqdm
 
 import xarray as xr
+import dask
+import dask.array as da
+
+from astropy.visualization import ImageNormalize, AsinhStretch
+
+from tqdm.dask import TqdmCallback
 
 # Initialize Python Logger
 logging.basicConfig(
@@ -24,15 +30,53 @@ LOG.setLevel(logging.INFO)
 
 
 class ZarrToJpg:
-    """ """
+    """
+    Class containing the methods necessary to create jpgs out of AIA data. It includes means
+    for automatically setting asinh normalization
+
+    Parameters
+    ----------
+    aia_path : str
+        Path pointing to Zarr array
+    stack_outpath : str
+        Path to place the folder containing output
+    wavelength_order : list, optional
+        List with wavelengths to place in the red, green, and blue channels, by default [211, 193, 171]
+    debug : bool, optional
+        flag that uses a reduced version of the array, only 10 samples, by default False
+    hist_low_lim : float, optional
+        Low limit in histogram calucation, by default 0
+    hist_high_lim : float, optional
+        High limit in histogram calculation, by default 1000
+    hist_delta : float, optional
+        Size of the histogram bin, by default 0.1
+    vmax_percentile : float, optional
+        Max percentile to be used to set vmax in the archsinstretch, by default 99
+    vmax_factor : float, optional
+        Additional stretch that can help with very bright active regions, by default 2.5
+    stretch_percentile : float, optional
+        Percentile that will be pegged to a stretch position after stretch, by default 40
+    stretch_position : float, optional
+        Stretch position to wich the percentile above will be mapped, by default 0.4
+    """
 
     def __init__(
         self,
-        aia_path="/d0/euv/aia/preprocessed/aia_hmi_stacks_2010_2023_1d_full.zarr",
-        stack_outpath="/d0/euv/aia/preprocessed",
-        wavelength_order=[211, 193, 171],
-        debug=False,
+        aia_path: str,
+        stack_outpath: str,
+        wavelength_order: list = [211, 193, 171],
+        debug: bool = False,
+        hist_low_lim: float = 0,
+        hist_high_lim: float = 1000,
+        hist_delta: float = 0.1,
+        vmax_percentile: float = 99,
+        vmax_factor: float = 2.5,
+        stretch_percentile: float = 40,
+        stretch_position: float = 0.4,
     ):
+        assert (
+            len(wavelength_order) == 3
+        ), "Three channels need to be provided to generate jpgs."
         self.aia_path = aia_path
         self.stack_outpath = (
             stack_outpath + "/AIA_" + "_".join([str(wl) for wl in wavelength_order])
@@ -49,9 +93,73 @@ class ZarrToJpg:
         if not os.path.exists(self.stack_outpath):
             os.mkdir(self.stack_outpath)
 
-    def save_jpg(self, index):
-        aia_stack = np.arcsinh(self.aia_slice[index, :, :, :].load().data)
-        aia_stack = aia_stack.transpose(1, 2, 0)
+        # Calculate histogram of pixel values for each channel in the slice
+        histogram_dict = {}
+        histogram_dict["bins"] = np.arange(hist_low_lim, hist_high_lim, hist_delta)
+        for i, channel in enumerate(self.aia_slice.channel):
+            histogram_dict[str(channel.data)], _ = da.histogram(
+                self.aia_slice.loc[:, str(channel.data), :, :].data,
+                bins=histogram_dict["bins"],
+            )
+
+        with TqdmCallback(desc="Calculating data histogram"):
+            self.histogram_dict = dask.compute(histogram_dict)[0]
+
+        # Calculate cumulative distribution for each channel in the slice to calculate percentiles
+        self.cumsum_dict = {}
+        for i, channel in enumerate(self.aia_slice.channel):
+            self.cumsum_dict[str(channel.data)] = (
+                np.cumsum(self.histogram_dict[str(channel.data)])
+                / np.sum(self.histogram_dict[str(channel.data)])
+                * 100
+            )
+
+        # Calculate channel percentiles to set normalization
+        percentile_list = [stretch_percentile, vmax_percentile]
+        self.percentile_dict = get_percentiles(
+            self.cumsum_dict,
+            self.histogram_dict["bins"],
+            percentile_list=percentile_list,
+        )
+
+        self.sdo_asinh_norms = {}
+        for i, channel in enumerate(self.aia_slice.channel):
+            test_stretch = 0.25
+            for n in range(3, 15):
+                test_norm = ImageNormalize(
+                    vmin=0,
+                    vmax=self.percentile_dict[str(channel.data)][1],
+                    stretch=AsinhStretch(test_stretch),
+                    clip=False,
+                )
+                test_val = test_norm(self.percentile_dict[str(channel.data)][0])
+                if test_val < stretch_position:
+                    test_stretch = test_stretch - 1 / 2**n
+                else:
+                    test_stretch = test_stretch + 1 / 2**n
+
+            self.sdo_asinh_norms[str(channel.data)] = ImageNormalize(
+                vmin=0,
+                vmax=self.percentile_dict[str(channel.data)][1] * vmax_factor,
+                stretch=AsinhStretch(test_stretch),
+                clip=False,
+            )
+
+    def save_jpg(self, index: int):
+        """Functin that saves a jpg for a given index of the Zarr array
+
+        Parameters
+        ----------
+        index : int
+            numerical index to save
+        """
+        aia_stack = self.aia_slice[index, :, :, :].load()
+        for i, channel in enumerate(aia_stack.channel):
+            aia_stack.loc[channel, :, :] = self.sdo_asinh_norms[str(channel.data)](
+                aia_stack.loc[channel, :, :]
+            )
+
+        aia_stack = aia_stack.data.transpose(1, 2, 0)
         aia_stack[aia_stack < 0] = 0
         aia_stack[aia_stack > 1] = 1
         output_file = (
@@ -65,35 +173,147 @@ class ZarrToJpg:
             os.path.join(self.stack_outpath, output_file), aia_stack, vmin=0, vmax=1
         )
 
+    def save_jpgs(self):
+        """
+        Method that sets up the delayed actions to save all jpgs and executes them
+        """
+
+        # Create list
+        delayed_list = []
+
+        for i in range(self.aia_slice.shape[0]):
+            delayed_list.append(dask.delayed(self.save_jpg)(i))
+
+        with TqdmCallback(desc="Saving jpgs"):
+            dask.compute(*delayed_list)
+
+
+def get_percentiles(
+    cumsum_dic: dict, bins: np.array, percentile_list: list = [80, 90]
+) -> dict:
+    """Function that gets percentile values based on a dictionary of histogram cumulative sums
+
+    Parameters
+    ----------
+    cumsum_dic : dict
+        Dictionary with cumulative sums
+    bins : np.array
+        Bins used for the calibration of the cumulative sums
+    percentile_list : list, optional
+        List of percentiles to calculate, by default [80, 90]
+
+    Returns
+    -------
+    dict
+        Dictionary with the calculated percentiles.  One for each AIA wavelength
+    """
+    percentile_dict = {}
+    for wl, cumsum in cumsum_dic.items():
+        # print(wl, cumsum)
+        value_list = []
+        for percentile in percentile_list:
+            # print(cumsum<=percentile)
+            # print(np.sum(cumsum<=percentile))
+            index = np.sum(cumsum <= percentile)
+            low_value = 0
+            if index > 0:
+                low_value = cumsum[index - 1]
+            high_value = cumsum[index]
+            # print(bins[index], bins[index+1], low_value, high_value)
+            interpolated_value = bins[index] + (bins[index + 1] - bins[index]) * (
+                percentile - low_value
+            ) / (high_value - low_value)
+            # print(interpolated_value)
+            value_list.append(interpolated_value)
+
+        percentile_dict[wl] = value_list
+
+    return percentile_dict
+
 
 def parse_args():
     # Commands
     p = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     p.add_argument(
-        "--aia_path", dest="aia_path", type=str, default="/mnt/data", help="aia_path"
+        "--aia_path",
+        dest="aia_path",
+        type=str,
+        default="/mnt/data.zarr",
+        help="Path to zarr file",
     )
     p.add_argument(
         "--stack_outpath",
         dest="stack_outpath",
         type=str,
         default="/mnt/data_out",
-        help="out_path",
-    )
-    p.add_argument(
-        "--file_format",
-        dest="file_format",
-        type=str,
-        default="npy",
-        help="format to save the stack in",
+        help="Path for jpg output",
     )
     p.add_argument(
         "--wavelength_order",
+        dest="wavelength_order",
         type=str,
         nargs="+",
         default=None,
         help="Order in which to stack the files, needs to contain only available wavelengths",
     )
     p.add_argument("--debug", action="store_true", help="Only process a few files (10)")
+
+    p.add_argument(
+        "--hist_low_lim",
+        dest="hist_low_lim",
+        type=float,
+        default=0,
+        help="Low limit in histogram calucation, by default 0",
+    )
+
+    p.add_argument(
+        "--hist_high_lim",
+        dest="hist_high_lim",
+        type=float,
+        default=1000,
+        help="High limit in histogram calculation, by default 1000",
+    )
+
+    p.add_argument(
+        "--hist_delta",
+        dest="hist_delta",
+        type=float,
+        default=0.1,
+        help="Size of the histogram bin, by default 0.1",
+    )
+
+    p.add_argument(
+        "--vmax_percentile",
+        dest="vmax_percentile",
+        type=float,
+        default=99,
+        help="Max percentile to be used to set vmax in the archsinstretch, by default 99",
+    )
+
+    p.add_argument(
+        "--vmax_factor",
+        dest="vmax_factor",
+        type=float,
+        default=2.5,
+        help="Additional stretch that can help with very bright active regions, by default 2.5",
+    )
+
+    p.add_argument(
+        "--stretch_percentile",
+        dest="stretch_percentile",
+        type=float,
+        default=40,
+        help="Percentile that will be pegged to a stretch position after stretch, by default 40",
+    )
+
+    p.add_argument(
+        "--stretch_position",
+        dest="stretch_position",
+        type=float,
+        default=0.4,
+        help="Stretch position to wich the percentile above will be mapped, by default 0.4",
+    )
+
     args = p.parse_args()
     return args
 
@@ -105,5 +325,27 @@ if __name__ == "__main__":
     stack_outpath = args.stack_outpath
     wavelength_order = args.wavelength_order
     debug = args.debug
+    hist_low_lim = args.hist_low_lim
+    hist_high_lim = args.hist_high_lim
+    hist_delta = args.hist_delta
+    vmax_percentile = args.vmax_percentile
+    vmax_factor = args.vmax_factor
+    stretch_percentile = args.stretch_percentile
+    stretch_position = args.stretch_position
 
     # open zarr
+    zarr_to_jpg = ZarrToJpg(
+        aia_path=aia_path,
+        stack_outpath=stack_outpath,
+        wavelength_order=wavelength_order,
+        debug=debug,
+        hist_low_lim = hist_low_lim,
+        hist_high_lim = hist_high_lim,
+        hist_delta = hist_delta,
+        vmax_percentile = vmax_percentile,
+        vmax_factor = vmax_factor,
+        stretch_percentile = stretch_percentile,
+        stretch_position = stretch_position,
+    )
+
+    zarr_to_jpg.save_jpgs()
